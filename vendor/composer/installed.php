7f70059f2f6dcb0944fc9add650e2c22977fcba1
Артем Фесенко <fesenko_a@bsu.edu.ru>
2022-05-04 20:24:41 +0300

    Файлы загрузки композера

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '2e1df3b622f08ac4bb40f517d9159424fce42ab5',
+        'name' => 'artem/abitur-web',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'artem/abitur-web' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '2e1df3b622f08ac4bb40f517d9159424fce42ab5',
+            'dev_requirement' => false,
+        ),
+    ),
+);
