a8f06c51a192b14f13456eaaba7126593562a79f
Артем Фесенко <fesenko_a@bsu.edu.ru>
2022-05-04 15:36:17 +0300

    Базовый класс контейнера зависимостей

---
+++ vendor/DI/DI.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Vendor\DI;
+
+class DI
+{
+    public function get($key): void
+    {
+        
+    }
+
+    public function set($key,$value): void
+    {
+        
+    }
+
+    public function has(): void
+    {
+        
+    }
+}
