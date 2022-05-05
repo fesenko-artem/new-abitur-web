36746ac98db4fb259e2eacb6044f58a7ae587f09
Артем Фесенко <fesenko_a@bsu.edu.ru>
2022-05-05 23:32:40 +0300

    Домашний контроллер корневого окружения

--- pub/Controller/HomeController.php
+++ pub/Controller/HomeController.php
@@ -6,6 +6,10 @@ class HomeController extends PubController
 {
     public function index()
     {
+        print_r($this->server->server);
+        echo '<br>';
+        print_r($this->session->session);
+        echo '<br>';
         $this->view->render('main',$this->data);
     }
 }
