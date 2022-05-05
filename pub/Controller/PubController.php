29cfd238624c712929b4038a79f2ce66b5043e01
Артем Фесенко <fesenko_a@bsu.edu.ru>
2022-05-05 23:32:19 +0300

    Основной контроллер корневого окружения

--- pub/Controller/PubController.php
+++ pub/Controller/PubController.php
@@ -50,20 +50,21 @@ class PubController extends Controller
         */
         $this->db = $this->di->get('db');
         $this->queryBuilder = $this->di->get('queryBuilder');
-        $this->config = $this->di->get('config');
         $this->load = $this->di->get('load');
         $this->csrf = $this->di->get('csrf');
         $this->view = $this->di->get('view');
         $this->auth = $this->di->get('auth');
+        $this->files = $this->di->get('files');
         $this->app_settings = $this->di->get('app_settings');
         $this->data['csrf'] = $this->csrf->setCSRF();
+        $this->session->set('CSRF',$this->data['csrf']);
         $this->data['errors'] = [];
         $this->forms = $this->di->get('forms');
         $this->cache = $this->di->get('cache');
         $this->data['env'] = mb_strtolower(ENV);
         $this->data['current_domain'] = $this->request->server['SERVER_NAME'];
         $this->data['app_config'] = $this->config->group('main');
-        $this->files = $this->di->get('files');
+
         /*
         if ($this->request->session['user_data']->role->name === 'moderator' & isset($this->request->session['SHADOW'])){
             $this->data['user_data'] = $this->request->session['SHADOW'];
