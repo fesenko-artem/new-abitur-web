984f2167bbc8cfc8c594407d916692e09f2d2e50
Артем Фесенко <fesenko_a@bsu.edu.ru>
2022-05-10 14:43:57 +0300

    Изменен компонент авторизации, добавлена возможность подгрузки библиотеки для авторизации

--- vendor/Core/Auth/Auth.php
+++ vendor/Core/Auth/Auth.php
@@ -6,28 +6,30 @@ use Vendor\Core\Config\Config;
 use Vendor\Helper\Session;
 class Auth
 {
-    public function fill_base_session()
+    public $auth_config;
+    public $auth_driver;
+    public function __construct()
     {
-        $_SESSION = [
-            'AUTH_STATUS'=>'N',
-            'LANG_CODE' => Config::item('app_lang','main'),
-            'IP_ADDR' => $_SERVER['REMOTE_ADDR'],
-        ];
-        return $_SESSION;
+        $this->auth_config = Config::group('Auth');
+        $namespace = '\\Vendor\\Lib\\Auth\\'. $this->auth_config['auth.driver'];
+        $this->auth_driver = new $namespace($this->auth_config);
     }
-    public function update_base_session(array $session_new_data)
+
+    public function auth($param_type,$param_value,$password)
     {
-        foreach ($session_new_data as $key => $value){
-            $_SESSION[$key] = $value;
+        $check = $this->auth_driver->check($param_type,$param_value,$password);
+        if ($check['status']){
+            $this->authorize($check['data']);
         }
-        return true;
+        return $check;
     }
-    public function authorize($user_data)
+
+    public function authorize($data)
     {
-        Session::set('AUTH_STATUS','Y');
-        Session::set('user_data', $user_data);
+        return $data;
     }
-    public function logout()
+
+    public function unauthorize()
     {
         session_destroy();
         header('Location: /');
