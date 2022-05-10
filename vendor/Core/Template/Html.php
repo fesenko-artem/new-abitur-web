9152db37f4701186130c4b08376ad79062f3f281
Артем Фесенко <fesenko_a@bsu.edu.ru>
2022-05-10 14:50:31 +0300

    Компонент для генерации html компонентов

--- vendor/Core/Template/Html.php
+++ vendor/Core/Template/Html.php
@@ -4,5 +4,44 @@ namespace Vendor\Core\Template;
 
 class Html
 {
+    public const DIV_OPEN_BASE_TAG = '<%s %s >';
+    public const INPUT_BASE_TAG = '<%s %s value="%s">';
+    public const DIV_CLOSE_BASE_TAG = '<%s %s>%s</%s>';
 
+    public function make_base_tag($tag_name,$attribute,$content,$component_template = self::DIV_CLOSE_BASE_TAG)
+    {
+        $base_component = '';
+        switch ($component_template){
+            case self::DIV_CLOSE_BASE_TAG:
+                if ($tag_name == 'select'){
+                    $content = $this->prepare_select_options($content);
+                }
+                $base_component = sprintf($component_template,$tag_name,$this->prepare_attribute($attribute),$content,$tag_name);
+                break;
+            case self::DIV_OPEN_BASE_TAG:
+                $base_component = sprintf($component_template,$tag_name,$this->prepare_attribute($attribute));
+                break;
+            case self::INPUT_BASE_TAG:
+                $base_component = sprintf($component_template,$tag_name,$this->prepare_attribute($attribute),$content);
+                break;
+            default: break;
+        }
+        return $base_component;
+    }
+    public function prepare_attribute($attribute_list)
+    {
+        $result = '';
+        foreach ($attribute_list as $attribute_key=>$attribute_value){
+            $result .= sprintf('%s="%s" ',$attribute_key,$attribute_value);
+        }
+        return $result;
+    }
+    public function prepare_select_options($option_list=[])
+    {
+        $result = '';
+        foreach ($option_list as $option_key => $option_value){
+            $result .= sprintf('<option value="%s">%s</option>',$option_key,$option_value);
+        }
+        return $result;
+    }
 }
--- vendor/Core/Template/HtmlComponent.php
+++ vendor/Core/Template/HtmlComponent.php
@@ -4,5 +4,34 @@ namespace Vendor\Core\Template;
 
 class HtmlComponent extends Html
 {
+    public function form_floating($type,$identifier,$label_text,$placeholder)
+    {
+        return $this->make_base_tag(
+            'div',
+            ['class'=>'form-floating'],
+            $this->make_base_tag(
+                'input',
+                ['type'=>$type,'class'=>'form-control','id'=>$identifier,'name'=>$identifier,'placeholder'=>$placeholder],
+                '',
+                $this::INPUT_BASE_TAG
+            ).
+            $this->make_base_tag(
+                'label',
+                ['for'=>$identifier],
+                $label_text,
+                $this::DIV_CLOSE_BASE_TAG
+            ),
+            $this::DIV_CLOSE_BASE_TAG
+        );
+    }
 
+    public function input_group($base_classes,$content)
+    {
+        return $this->make_base_tag(
+            'div',
+            ['class'=>'input-group '.$base_classes],
+            $content,
+            $this::DIV_CLOSE_BASE_TAG
+        );
+    }
 }
