c72e6934f16ac83d6c642b54d2d5c444fba0acf5
Артем Фесенко <fesenko_a@bsu.edu.ru>
2022-05-16 01:02:39 +0300

    Рефакторинг дублирующегося кода, подстановка в сессию запрашиваемого URL

--- developer/Controller/DeveloperController.php
+++ developer/Controller/DeveloperController.php
@@ -7,24 +7,37 @@ use Vendor\DI\DI;
 
 class DeveloperController extends Controller
 {
-    protected $db;
-    protected $config;
-    protected $load;
-    protected $queryBuilder;
-    protected $view;
-    protected $app_settings;
-    protected $cache;
-    protected $forms;
-    protected $csrf;
-    protected $auth;
-    protected $data;
-    protected $files;
-    protected $datetime;
     public function __construct(DI $di)
     {
         parent::__construct($di);
+        $this->session->set('LAST_POSITION',$this->server->get('REQUEST_URI'));
         if ($this->session->get('AUTH') == 'N'){
             $this->request->redirect('/auth/');
         }
+        $this->check_access();
+    }
+
+    public function check_access()
+    {
+        $this->load->model('Environment');
+        $this->load->model('EnvironmentAccess');
+        $this->data['current_environment'] = $this->model->environment->getEnvironmentByParam('env_name',mb_strtolower((ENV)));
+        $this->data['roles_list'] = $this->model->environmentAccess->getEnvironmentAccessGroupsByEnvId($this->data['current_environment']->id);
+        $status = false;
+        foreach ($this->session->get('ROLE_LIST') as $role_key=>$role_value){
+            foreach ($this->data['roles_list'] as $key=>$value){
+                if ($role_value->id == $value->group_id){
+                    $status = true;
+                    break;
+                }
+            }
+        }
+        if (!$status){
+            $this->request->redirect('/auth/');
+            exit;
+        }
+        $this->load->model('EnvironmentLink');
+        $this->data['environment_links'] = $this->model->environmentLink->getEnvironmentLinkByParam('environment_id',$this->data['current_environment']->id);
+        return true;
     }
 }
