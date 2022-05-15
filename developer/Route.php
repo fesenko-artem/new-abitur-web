c0e59dd11dfa617355fa033295dbaeda87b4224e
Артем Фесенко <fesenko_a@bsu.edu.ru>
2022-05-16 00:59:58 +0300

    Добавлены маршруты для работы с приложениями, и апи

--- developer/Route.php
+++ developer/Route.php
@@ -1,2 +1,7 @@
 <?php
 $this->router->add('developer-index','/developer/','HomeController:index');
+$this->router->add('environments-list','/developer/environments','EnvironmentsController:list');
+$this->router->add('environment-info','/developer/environment/show/(id:int)','EnvironmentsController:show');
+$this->router->add('environment-file','/developer/environment/file/(id:int)','EnvironmentsController:file');
+$this->router->add('developer-api-get','/developer/api/(request_method:any)/(method:any)','DeveloperApiController:api');
+$this->router->add('developer-api-post','/developer/api/(request_method:any)/(method:any)','DeveloperApiController:api','POST');
