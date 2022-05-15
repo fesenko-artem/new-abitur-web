0135958ea9e2d24c8ac33b08aa8b390bc770b7cc
Артем Фесенко <fesenko_a@bsu.edu.ru>
2022-05-16 01:01:04 +0300

    Добавленно подключение навигации базового окружения

--- pub/content/html/main.php
+++ pub/content/html/main.php
@@ -1,13 +1,3 @@
 <?= $this->theme->block('header'); ?>
-<div class="d-flex">
-    <?= $this->theme->block('offcanvas-sidebar'); ?>
-    <div class="d-flex flex-column justify-content-center w-100"><br>
-        <div class="container-fluid">
-            <?php foreach ($app_config as $app_config_key=>$app_config_value):?>
-            <?= $app_config_key.' => '.$app_config_value ?>
-            <br>
-            <?php endforeach; ?>
-        </div>
-    </div>
-</div>
+<?= $this->theme->block('standart-navbar'); ?>
 <?= $this->theme->block('footer'); ?>
