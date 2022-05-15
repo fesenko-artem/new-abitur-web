14d44cd121e0a43a380e47cd7bff90331f2a8074
Артем Фесенко <fesenko_a@bsu.edu.ru>
2022-05-16 01:01:47 +0300

    Рефакторинг дублирующегося кода

--- pub/Controller/PubController.php
+++ pub/Controller/PubController.php
@@ -7,21 +7,9 @@ use Vendor\DI\DI;
 
 class PubController extends Controller
 {
-    protected $db;
-    protected $config;
-    protected $load;
-    protected $queryBuilder;
-    protected $view;
-    protected $app_settings;
-    protected $cache;
-    protected $forms;
-    protected $csrf;
-    protected $auth;
-    protected $data;
-    protected $files;
-    protected $datetime;
     public function __construct(DI $di)
     {
         parent::__construct($di);
+        $this->session->set('LAST_POSITION',$this->server->get('REQUEST_URI'));
     }
 }
